f47e0dd184eabe0221551cde0fdb11150274fd96
GEPIDEN <code@cisowscy.com>
2026-01-21 22:13:26 +0100

    konfiguracja na android

---
+++ src-tauri/gen/android/app/src/main/java/com/gepiden/lekcja003/kalkulator/gui/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gepiden.lekcja003.kalkulator.gui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
